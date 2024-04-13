55a421eb9c8bcb09acfee5f198caad64f8c2890d
IzawaRiel <1944366169@qq.com>
2024-04-13 17:39:25 +0800

    添加resource、prefab和model

---
+++ Src/Client/Assets/Login.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Network.NetClient.Instance.Init("172.41.94.76", 8000);
+		Network.NetClient.Instance.Connect();
+
+		SkillBridge.Message.NetMessage msg=new SkillBridge.Message.NetMessage();
+		msg.Request=new SkillBridge.Message.NetMessageRequest();
+
+		Network.NetClient.Instance.SendMessage(msg);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
